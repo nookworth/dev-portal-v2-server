91ab54d06b43c1577abb8e22bc43dfdc0d0072ad
nookworth <nooksack@gmail.com>
2025-01-06 23:54:37 -0700

    extract intermediate functions into utils file

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import { WebSocketServer } from 'ws'
 import http from 'node:http'
-import axios from 'axios'
-import { OWNER, REPO, BASE_URL } from './constants.js'
+import { getPRs } from './utils.js'
 import { WebClient } from '@slack/web-api'
 import bodyParser from 'body-parser'
 import 'dotenv/config'
@@ -15,92 +14,6 @@ const server = http.createServer(app)
 const wss = new WebSocketServer({ server })
 
 app.use(bodyParser.text())
-
-const getCheckSuitesForCommit = async sha => {
-  const checkSuiteUrl = `${BASE_URL}/repos/${OWNER}/${REPO}/commits/${sha}/check-suites`
-  const response = await axios.get(checkSuiteUrl, {
-    headers: {
-      Accept: 'application/vnd.github+json',
-      'X-GitHub-Api-Version': '2022-11-28',
-    },
-  })
-  const status = response?.status
-
-  if (status === 200) {
-    const checkResults = []
-    const checkSuites = response?.data?.check_suites
-    for (const { id, conclusion, status } of checkSuites) {
-      checkResults.push({ id, conclusion, status })
-    }
-    return checkResults
-  } else {
-    throw new Error(`Failed to fetch check suite data for ${sha}`)
-  }
-}
-
-/**@description this may be needed when working with travelpass pull requests */
-// const getStatusOfCommit = async sha => {
-//   const statusURL = `${BASE_URL}/repos/${OWNER}/${REPO}/commits/${sha}/status`
-//   const response = await axios.get(statusURL, {
-//     headers: {
-//       Accept: 'application/vnd.github+json',
-//       'X-GitHub-Api-Version': '2022-11-28',
-//     },
-//   })
-//   const status = response?.status
-//   if (status === 200) {
-//     const state = response?.data?.state
-//     return state
-//   } else {
-//     throw new Error(`Failed to fetch status for ${sha}`)
-//   }
-// }
-
-/**@todo this should fetch all of the logged-in user's PRs */
-const getPRs = async () => {
-  const allPRsURL =
-    process.env.URL || `${BASE_URL}/repos/${OWNER}/${REPO}/pulls`
-
-  try {
-    const response = await axios.get(allPRsURL, {
-      headers: {
-        Accept: 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    })
-    const status = response?.status
-
-    if (status === 200) {
-      const commitShas = []
-      const prData = response?.data?.map(pr => ({
-        head: pr.head,
-        number: pr.number,
-        title: pr.title,
-      }))
-      if (prData?.length) {
-        for await (const {
-          head: { sha },
-          number,
-          title,
-        } of prData) {
-          // const commitStatus = await getStatusOfCommit(sha)
-          const checkSuites = await getCheckSuitesForCommit(sha)
-
-          commitShas.push({
-            checkSuites,
-            number,
-            title,
-          })
-        }
-      }
-      return commitShas
-    } else {
-      throw new Error('Failed to fetch PRs')
-    }
-  } catch (e) {
-    console.error(e)
-  }
-}
 
 // the frontend fetches PRs from this route
 app.get('/', async (_, res) => {
