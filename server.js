edf46d9800168876575b0eb05b4ba86a426269fa
nookworth <nooksack@gmail.com>
2025-01-19 17:23:25 -0700

    modify port

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ wss.on('connection', ws => {
   })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
