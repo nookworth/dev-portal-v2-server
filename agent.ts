e05fb80a562d8f2386507c898a9e3582df7a5a4d
nookoid <christopher.morrison@travelpassgroup.com>
2025-03-04 13:43:57 -0700

    some pseudocode about the agent

--- agent.ts
+++ agent.ts
@@ -6,3 +6,24 @@ import { ChatOpenAI } from '@langchain/openai'
 import { Tool } from '@langchain/core/tools'
 import { StringOutputParser } from '@langchain/core/output_parsers'
 import { AgentExecutor, createToolCallingAgent } from 'langchain/agents'
+
+/**
+ * need a tool that takes care of the initial deployment process
+ * not sure if these two steps should be separate tools 
+ * 1. creates a PR from local head branch (?)
+ * 2. posts to Slack
+ * 
+ * do I need a db of timestamps to be able to delete slack posts in the future? or is it OK to stick with the user data file?
+ */
+
+/**
+ * tool to delete slack posts
+ */
+
+/**
+ * tool to close PR 
+ */
+
+/**
+ * tool to merge PR
+ */
